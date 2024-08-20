ae9f60f6e3ce6fbfb7c5b365903ed186e056d2ee
“Daoist <kelvinjose04@gmail.com>
2024-08-20 18:55:42 -0500

    fix

--- scripts/productcards.js
+++ scripts/productcards.js
@@ -1,25 +1,29 @@
-// Seleccionar el contenedor de productos
-const productsSelector = document.getElementById("products");
 
-// Definir el template para las tarjetas de productos
-let productsTemplate = `
-  ${products.map(product => `
-    <article class="product-card">
-      <img class="product-img" src="${product.imageSrc}" alt="${product.title}" />
-      <div class="product-info">
-        <span class="product-title">${product.title}</span>
-        <span class="product-description">${product.description}</span>
-        <div class="product-price-block">
-          <span class="product-price">${product.price}</span>
-          <span class="product-discount">${product.discount}</span>
-        </div>
-        <div class="product-tax-policy">
-          Incluye impuesto País y percepción AFIP
-        </div>
-      </div>
-    </article>
-  `).join('')}
-`;
+// Función para crear una tarjeta de producto
+function createCard(product) {
+  return `
+      <a class="product-card" href="./details.html?id=${product.id}">
+          <img class="product-img" src="${product.imageSrc}" alt="${product.title}" />
+          <div class="product-info">
+              <span class="product-title">${product.title}</span>
+              <span class="product-description">${product.description}</span>
+              <div class="product-price-block">
+                  <span class="product-price">${product.price}</span>
+                  <span class="product-discount">${product.discount}</span>
+              </div>
+              <div class="product-tax-policy">
+                  Incluye impuesto País y percepción AFIP
+              </div>
+          </div>
+      </a>
+  `;
+}
 
-// Insertar el template en el contenedor de productos
-productsSelector.innerHTML = productsTemplate;
+// Función para actualizar la vista de productos
+function updateProductView(products) {
+  const productsSelector = document.getElementById("products");
+  if (productsSelector) {
+      const productsTemplate = products.map(createCard).join('');
+      productsSelector.innerHTML = productsTemplate;
+  }
+}
